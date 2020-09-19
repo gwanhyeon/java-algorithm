0018932c0ccb82e55df38a14dc391191114aeed4
gwanhyeon <kgh940525@gmail.com>
2020-09-19 13:34:24 +0900

    Add 문자열내 p와yr개수, 문자열 내림차순 배치

---
+++ src/main/java/문자열내p와y의개수.java
@@ -0,0 +1,26 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class 문자열내p와y의개수 {
+    public static void main(String[] args) {
+
+        String s = "pPoooyY";
+        s = s.toLowerCase();
+        Boolean answer = false;
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++){
+
+            if(s.charAt(i) == 'p'){
+                cnt++;
+            }else if(s.charAt(i) == 'y'){
+                cnt--;
+            }
+            if(i == s.length()-1){
+                if(cnt == 0){
+                    answer = true;
+                }
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
