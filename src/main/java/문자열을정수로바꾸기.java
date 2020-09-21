0847b9e62959e7171a59f0003dbc298168af44af
gwanhyeon <kgh940525@gmail.com>
2020-09-21 19:48:44 +0900

    Add 문자열을 정수로 바꾸기, 수박수박수, 시저암호

---
+++ src/main/java/문자열을정수로바꾸기.java
@@ -0,0 +1,10 @@
+public class 문자열을정수로바꾸기 {
+    public static void main(String[] args) {
+        String s ="1234";
+
+        int answer = Integer.parseInt(s);
+        System.out.println(answer);
+
+    }
+
+}
