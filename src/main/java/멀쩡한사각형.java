4f7ba62e2489885e75053f7c233c97a91987abdc
gwanhyeon <kgh940525@gmail.com>
2020-09-23 18:15:00 +0900

    Add 멀쩡한 사각형, 숫자카드재귀

---
+++ src/main/java/멀쩡한사각형.java
@@ -0,0 +1,29 @@
+public class 멀쩡한사각형 {
+
+    static int gcd(int w,int h){
+
+
+        if(h == 0){
+            return w;
+        }
+        return gcd(h, w % h);
+    }
+    public static void main(String[] args) {
+
+        int w = 8;
+        int h = 12;
+
+        int gcd_num = gcd(w,h);
+        System.out.println(gcd_num);
+
+        long squreArea = (long)w * (long)h;
+        System.out.println(squreArea);
+        long answer = squreArea - ((long)w + (long )h) + gcd_num;
+        System.out.println(answer);
+
+
+
+
+
+    }
+}
