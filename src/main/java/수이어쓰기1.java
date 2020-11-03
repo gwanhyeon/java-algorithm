07d1ca988158683198b83d5298b1219701fda684
gwanhyeon <kgh940525@gmail.com>
2020-11-03 19:27:38 +0900

    Add 색종이, 수이어쓰기

---
+++ src/main/java/수이어쓰기1.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+자리수 찾기문제
+1234567891011 와같이 수를 이어붙이면 브루트포스로 처리하면
+ */
+public class 수이어쓰기1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long ans = 0;
+        int len = 1;
+        for(int i=1; i<=n; i*=10){
+            int j = i*10-1;
+            if(j > n){
+                j = n;
+            }
+            ans += (long)(j - i + 1) * len;
+            len++;
+        }
+        System.out.println(ans);
+    }
+}
