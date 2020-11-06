45ebeaee6a3756d71f8e7ae191b3c2655d86d18e
gwanhyeon <kgh940525@gmail.com>
2020-11-06 20:29:17 +0900

    Add 스킬트리, Two Sum, Valid Parentheses

---
+++ src/main/java/Study/leetcode_ValidParentheses_kgh.java
@@ -0,0 +1,81 @@
+package Study;
+import java.util.*;
+
+/*
+소요시간 30분
+ */
+
+/* 실제풀이 */
+public class leetcode_ValidParentheses_kgh {
+    public static void main(String[] args) {
+        System.out.println(isValidStack("([)]"));
+        System.out.println(isValidHash("((()))"));
+    }
+    /* Stack으로 푸는 방식 */
+    static boolean isValidStack(String s) {
+
+        Stack<Character> s1 = new Stack<>();
+        boolean tf = true;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('|| s.charAt(i) == '[' || s.charAt(i) == '{'){
+                s1.push(s.charAt(i));
+            }
+
+            if(s.charAt(i) == ')'){
+                if(!s1.isEmpty() && s1.peek() == '('){
+                    s1.pop();
+                }else {
+                    tf = false;
+                    break;
+                }
+            }else if(s.charAt(i) == '}'){
+                if(!s1.isEmpty() && s1.peek() == '{'){
+                    s1.pop();
+                }else {
+                    tf = false;
+                    break;
+                }
+
+            }else if(s.charAt(i) == ']'){
+                if(!s1.isEmpty() && s1.peek() == '['){
+                    s1.pop();
+                }else {
+                    tf = false;
+                    break;
+                }
+
+            }
+        }
+        // '[' 한문자 오는 경우 처리 해주기
+        if(s1.size() != 0){
+            tf = false;
+        }
+
+        return tf;
+    }
+    /* HashMap으로 푸는 방식 */
+    static boolean isValidHash(String s) {
+
+        HashMap<Character, Character> hm = new HashMap<>();
+        hm.put(')', '(');
+        hm.put(']', '[');
+        hm.put('}', '{');
+        boolean tf = false;
+        Stack<Character> s1 = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(hm.containsKey(s.charAt(i))){
+                if (s1.isEmpty() || s1.pop() != hm.get(s.charAt(i))) {
+                    return false;
+                }else{
+                    //System.out.print(s.charAt(i));
+                    s1.push(s.charAt(i));
+                }
+            }
+        }
+        return s.isEmpty();
+
+    }
+}
+
+
+
