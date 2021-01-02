aaa77b97de2020fc29493b95b22016065950ac12
gwanhyeon <kgh940525@gmail.com>
2021-01-02 23:00:52 +0900

    Add 완주하지 못한 선수

--- src/main/java/완주하지못한선수.java
+++ src/main/java/완주하지못한선수.java
@@ -4,9 +4,9 @@ import java.util.Map;
 public class 완주하지못한선수 {
     public static void main(String[] args) {
         Map<String,Integer> m = new HashMap<>();
-
         String[] participant = {"leo", "kiki", "eden"};
         String[] completion ={"eden", "kiki"};
+
         String answer = "";
         for(int i=0; i<completion.length; i++){
             if(!m.containsKey(completion[i])){
@@ -18,20 +18,54 @@ public class 완주하지못한선수 {
         }
 
         for(int i=0; i<participant.length; i++){
-            // 완주자와 참가명단에서 못찾았으면
             if(!m.containsKey(participant[i])){
                 answer = participant[i];
                 break;
             }else {
-                // 동명이인일 경우 값을 하나씩 줄였을때 0보다 작으면 그 사람이 완주자, 동명이인 체크해주기
                 int tmp = m.get(participant[i]);
-                m.put(participant[i],tmp-1);
-                if(m.get(participant[i])< 0){
+                m.put(participant[i], tmp-1);
+                if(m.get(participant[i]) < 0){
                     answer = participant[i];
                     break;
                 }
             }
         }
+
+
+
+
+//
+//
+//
+//        Map<유형별.String,Integer> m = new HashMap<>();
+//
+//        유형별.String[] participant = {"leo", "kiki", "eden"};
+//        유형별.String[] completion ={"eden", "kiki"};
+//        유형별.String answer = "";
+//        for(int i=0; i<completion.length; i++){
+//            if(!m.containsKey(completion[i])){
+//                m.put(completion[i],1);
+//            }else {
+//                int tmp = m.get(completion[i]);
+//                m.put(completion[i], tmp+1);
+//            }
+//        }
+//
+//        for(int i=0; i<participant.length; i++){
+//            // 완주자와 참가명단에서 못찾았으면
+//            if(!m.containsKey(participant[i])){
+//                answer = participant[i];
+//                break;
+//            }else {
+//                // 동명이인일 경우 값을 하나씩 줄였을때 0보다 작으면 그 사람이 완주자, 동명이인 체크해주기
+//                int tmp = m.get(participant[i]);
+//                m.put(participant[i],tmp-1);
+//                if(m.get(participant[i])< 0){
+//                    answer = participant[i];
+//                    break;
+//                }
+//            }
+//        }
 
         System.out.println(answer);
 
