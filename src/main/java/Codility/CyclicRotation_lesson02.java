4f3d85cbe35cda74e13f3cb4055360a788b90e52
gwanhyeon <kgh940525@gmail.com>
2021-03-01 14:51:26 +0900

    Add Codility Binary Gap, Cyclic Rotation, FrogJmp, frogRiverOne, OddOccurences In Array, PermMissingElem

---
+++ src/main/java/Codility/CyclicRotation_lesson02.java
@@ -0,0 +1,33 @@
+package Codility;
+
+import java.util.LinkedList;
+
+/**
+ * linkedlist를 사용하여 뒤에있는값들을 shift해주면 간단하게 풀리는 문제.
+ *
+ */
+public class CyclicRotation_lesson02 {
+    public static void main(String[] args) {
+        solution(new int[]{3, 8, 9, 7, 6},3);
+    }
+    static int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        if(A.length == 0){
+            return A;
+        }
+        for(int i=0; i<A.length; i++){
+            linkedList.add(A[i]);
+        }
+        for(int i=0; i<K; i++){
+            int last = linkedList.removeLast();
+            linkedList.addFirst(last);
+        }
+        int[] answer = new int[linkedList.size()];
+        int idx = 0;
+        for (Integer integer : linkedList) {
+            answer[idx++] = integer;
+        }
+        return answer;
+    }
+}
