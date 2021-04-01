69e4aaeab2703925810d6c4414aa94b82f06d78e
gwanhyeon <kgh940525@gmail.com>
2021-04-01 20:19:19 +0900

    create programmers 피보나치수

---
+++ src/main/java/스터디/스터디_GN/스터디_GN_14_주차/피보나치수_kgh.java
@@ -0,0 +1,21 @@
+package 스터디.스터디_GN.스터디_GN_14_주차;
+
+public class 피보나치수_kgh {
+    public static void main(String[] args) {
+        solution(3);
+    }
+    static int solution(int n) {
+        int answer = 0;
+        int[] dp = new int[100001];
+        // 0 + 1 = 1
+        // 1 + 1 = 2
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        System.out.println(dp[n]);
+        return answer;
+    }
+}
