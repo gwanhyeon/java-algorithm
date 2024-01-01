45f95c20798ff72746577027fe53fe86df81a12f
kgh <gwanhyeon0525@gmail.com>
2024-01-01 23:55:29 +0900

    feat: 정수변경

---
+++ src/main/java/_2024/programmers/문자열정수로바꾸기.java
@@ -0,0 +1,16 @@
+package _2024.programmers;
+
+public class 문자열정수로바꾸기 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution());
+
+    }
+    static class Solution{
+        int solution(){
+            String s = "-1234";
+            return Integer.parseInt(s);
+        }
+
+    }
+}
---
+++ src/main/java/_2024/programmers/평균구하기.java
@@ -0,0 +1,19 @@
+package _2024.programmers;
+
+import java.util.Arrays;
+
+class 평균구하기 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{10, 20, 30}));
+
+    }
+    static class Solution{
+         public  double solution(int[] arr) {
+            double answer = 0;
+            answer = Arrays.stream(arr).sum();
+            return answer / arr.length;
+        }
+    }
+
+}
