92b91be7e6bbcc61803f1547e88a0b3b6ca0b26a
kgh <gwanhyeon0525@gmail.com>
2024-01-03 23:06:13 +0900

    feat: 자리수 뒤집기

---
+++ src/main/java/_2024/programmers/자릿수를뒤집어배열로만들기.java
@@ -0,0 +1,22 @@
+package _2024.programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 자릿수를뒤집어배열로만들기 {
+    public static void main(String[] args) {
+        long n = 12345;
+        StringBuilder sb = new StringBuilder(Long.toString(n));
+        String s= sb.reverse().toString();
+
+        int[] answer = new int[s.length()];
+        for(int i=0; i<s.length(); i++){
+            answer[i] = Character.getNumericValue(s.charAt(i));
+        }
+
+        for (int i : answer) {
+            System.out.println(i);
+        }
+
+    }
+}
