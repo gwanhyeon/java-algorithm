cb593e6c1b8e8ccbdb51799919286f2b1beb5962
kgh <gwanhyeon0525@gmail.com>
2024-01-08 23:32:44 +0900

    feat: 서울에서 김서방찾기

---
+++ src/main/java/_2024/programmers/서울에서김서방찾기.java
@@ -0,0 +1,15 @@
+package _2024.programmers;
+
+public class 서울에서김서방찾기 {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane","Kim"};
+        int answer = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if("Kim".equals(seoul[i])){
+                answer = i;
+            }
+        }
+        System.out.println("김서방은 " + answer + "에 있다");
+
+    }
+}
