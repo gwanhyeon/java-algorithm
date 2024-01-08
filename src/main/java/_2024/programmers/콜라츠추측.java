e111c353813453203611935349eafed7f6eb51ca
kgh <gwanhyeon0525@gmail.com>
2024-01-08 23:28:40 +0900

    콜라츠의추측

---
+++ src/main/java/_2024/programmers/콜라츠추측.java
@@ -0,0 +1,21 @@
+package _2024.programmers;
+
+public class 콜라츠추측 {
+    public static void main(String[] args) {
+        int n = 626331;
+        int answer = 0;
+        while(n > 1){
+            if(n%2 == 0){
+                n = n /2;
+            }else{
+                n = (n * 3)+1;
+            }
+            answer++;
+        }
+        if(answer != 1){
+            answer = -1;
+        }
+
+        System.out.println(answer);
+    }
+}
