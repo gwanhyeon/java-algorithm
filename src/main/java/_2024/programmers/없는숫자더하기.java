877e651e9eb692e3ef8aae7af37d4291c19664bd
kgh <gwanhyeon0525@gmail.com>
2024-01-08 23:56:48 +0900

    feat: 없는 숫자 더하기 stream

---
+++ src/main/java/_2024/programmers/없는숫자더하기.java
@@ -0,0 +1,18 @@
+package _2024.programmers;
+
+import java.util.Arrays;
+
+public class 없는숫자더하기 {
+    public static void main(String[] args) {
+        int[] match = {1,2,3,4,5,6,7,8,9};
+        int[] numbers = {1,2,3,4,6,7,8,0};
+
+        int answer = Arrays.stream(match)
+                .filter(num -> Arrays.stream(numbers).noneMatch(num2 -> num == num2))
+                .sum();
+
+        System.out.println(answer);
+
+
+    }
+}
