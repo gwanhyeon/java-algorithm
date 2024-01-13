7c27d7f36d8ae11c737a1edee6c9ca828f164bc0
kgh <gwanhyeon0525@gmail.com>
2024-01-13 18:37:19 +0900

    feat: boj 9093

---
+++ src/main/java/_2024/baekjoon/boj9093.java
@@ -0,0 +1,25 @@
+package _2024.baekjoon;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            StringBuilder sb = new StringBuilder();
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            while (st.hasMoreTokens()) {
+                StringBuilder s = new StringBuilder(st.nextToken().toString());
+                sb.append(s.reverse()+ " ");
+            }
+            System.out.print(sb.toString());
+            System.out.println();
+
+        }
+
+    }
+}
