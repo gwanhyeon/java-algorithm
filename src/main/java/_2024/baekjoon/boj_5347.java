46cf88840a6c7025d025b4c218d632cebe26f5c3
kgh <gwanhyeon0525@gmail.com>
2024-02-05 23:26:26 +0900

    feat: boj 5347

---
+++ src/main/java/_2024/baekjoon/boj_5347.java
@@ -0,0 +1,34 @@
+package _2024.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_5347 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+
+            long gcd = gcd(x,y);
+            long lcm = getLcm(gcd, x,y);
+            System.out.println(lcm);
+        }
+    }
+
+    private static long gcd(long n, long m) {
+        if(m == 0){
+            return n;
+        }
+        return gcd(m, n%m);
+    }
+    private static long getLcm(long gcd, long n, long m){
+        return gcd * (n / gcd) * (m / gcd);
+    }
+}
