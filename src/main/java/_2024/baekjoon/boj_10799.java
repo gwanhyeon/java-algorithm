d29f842fc7178c9be3b455ef2e6762934b810d8c
kgh <gwanhyeon0525@gmail.com>
2024-03-13 22:54:53 +0900

    feat: boj 10799

---
+++ src/main/java/_2024/baekjoon/boj_10799.java
@@ -0,0 +1,34 @@
+package _2024.baekjoon;
+
+import java.util.*;
+import java.io.*;
+public class boj_10799 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String blanks = br.readLine();
+
+        char[] c = blanks.toCharArray();
+
+        Stack<Character> s = new Stack<>();
+        int answer = 0;
+        for(int i=0; i<c.length; i++){
+            if(c[i] == ')' && !s.isEmpty() && s.peek() == '('){
+                s.pop();
+                if(c[i-1] == '('){
+                    answer += s.size();
+                }else {
+                    answer += 1;
+                }
+            }else {
+                if(c[i] == '(') {
+                    s.push(c[i]);
+                }
+
+            }
+        }
+        System.out.println(answer);
+
+
+    }
+}
